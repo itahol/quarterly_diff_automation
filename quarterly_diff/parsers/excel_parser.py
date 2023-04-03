75a85e4fc1d2bf6894ffbaf1ba93fb1902fceb99
Itamar <itamar@ivc-online.com>
2023-04-03 22:47:58 +0300

    Add currency support

--- quarterly_diff/parsers/excel_parser.py
+++ quarterly_diff/parsers/excel_parser.py
@@ -9,9 +9,10 @@ if TYPE_CHECKING:
 
 
 class CompanyInvestment(object):
-    def __init__(self, company_id: str, stake: float):
+    def __init__(self, company_id: str, stake: float, currency: str = "שקל חדש"):
         self._company_id = company_id
         self._stake = stake
+        self._currency = currency
 
     @property
     def company_id(self) -> str:
@@ -21,15 +22,25 @@ class CompanyInvestment(object):
     def stake(self) -> float:
         return self._stake
 
+    @property
+    def currency(self) -> str:
+        return self._currency
+
     def __sub__(self, other) -> CompanyInvestment:
         if other.company_id != self.company_id:
             raise ValueError(f"Different ID's! {self.company_id} != {other.company_id}")
-        return CompanyInvestment(company_id=self.company_id, stake=self.stake - other.stake)
+        elif other.currency != self.currency:
+            raise ValueError(f"Different currencies! {self.currency} != {other.currency}")
+
+        return CompanyInvestment(company_id=self.company_id, stake=self.stake - other.stake, currency=self.currency)
 
     def __add__(self, other) -> CompanyInvestment:
         if other.company_id != self.company_id:
             raise ValueError(f"Different ID's! {self.company_id} != {other.company_id}")
-        return CompanyInvestment(company_id=self.company_id, stake=self.stake + other.stake)
+        elif other.currency != self.currency:
+            raise ValueError(f"Different currencies! {self.currency} != {other.currency}")
+
+        return CompanyInvestment(company_id=self.company_id, stake=self.stake + other.stake, currency=self.currency)
 
     def __repr__(self) -> str:
         return f"<CompanyInvestment: {self._company_id}>"
@@ -53,6 +64,11 @@ class ExcelParser(metaclass=ABCMeta):
 
     @property
     @abstractmethod
+    def CURRENCY_COL(self):
+        pass
+
+    @property
+    @abstractmethod
     def STAKE_SHEET_NAME(self):
         pass
 
@@ -66,6 +82,9 @@ class ExcelParser(metaclass=ABCMeta):
     def _get_stake_at_company(self, investment):
         return investment[self.STAKE_AT_COMPANY_COL].value
 
+    def _get_currency(self, investment):
+        return investment[self.CURRENCY_COL].value
+
     @property
     def _investments_rows(self):
         return (self._sheet.row(index) for index in range(self.COMPANIES_START_ROW, self._sheet.nrows)
@@ -74,15 +93,17 @@ class ExcelParser(metaclass=ABCMeta):
     @property
     def investments(self) -> Generator[CompanyInvestment, None, None]:
         return (CompanyInvestment(company_id=self._get_company_id(investment),
-                                  stake=self._get_stake_at_company(investment)) for investment in
+                                  stake=self._get_stake_at_company(investment),
+                                  currency=self._get_currency(investment)) for investment in
                 self._investments_rows)
 
     @property
     def summed_investments(self) -> Iterator[CompanyInvestment, None, None]:
         companies_dict = {}
         for investment in self.investments:
-            companies_dict[investment.company_id] = companies_dict.get(investment.company_id,
-                                                                       CompanyInvestment(
-                                                                           company_id=investment.company_id,
-                                                                           stake=0)) + investment
+            companies_dict[(investment.company_id, investment.currency)] = companies_dict.get(
+                (investment.company_id, investment.currency),
+                CompanyInvestment(
+                    company_id=investment.company_id,
+                    stake=0, currency=investment.currency)) + investment
         return iter(companies_dict.values())
--- quarterly_diff/parsers/phoenix_parser.py
+++ quarterly_diff/parsers/phoenix_parser.py
@@ -4,5 +4,6 @@ from .excel_parser import ExcelParser
 class Phoenix(ExcelParser):
     COMPANIES_START_ROW = 13
     COMPANIES_ID_COL = 5
+    CURRENCY_COL = 7
     STAKE_AT_COMPANY_COL = 8
     STAKE_SHEET_NAME = "מניות"
