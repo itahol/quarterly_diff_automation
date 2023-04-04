737d949d23c9351dd0cad5ce63be58d879cbfb5a
Itamar <itamar@ivc-online.com>
2023-04-04 13:01:43 +0300

    Add some more parsers

--- quarterly_diff/parsers/__init__.py
+++ quarterly_diff/parsers/__init__.py
@@ -1,2 +1,4 @@
 from .excel_parser import ExcelParser
 from .phoenix_parser import Phoenix
+from .menora_parser import Menora
+from .altshuler_parser import Altshuler
---
+++ quarterly_diff/parsers/altshuler_parser.py
@@ -0,0 +1,9 @@
+from .excel_parser import ExcelParser
+
+
+class Altshuler(ExcelParser):
+    COMPANIES_START_ROW = 13
+    COMPANIES_ID_COL = 5
+    CURRENCY_COL = 7
+    STAKE_AT_COMPANY_COL = 8
+    STAKE_SHEET_NAME = "מניות"
