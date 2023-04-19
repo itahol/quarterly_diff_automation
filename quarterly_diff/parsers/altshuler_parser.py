f4afede5d13361def4dcb7b774fd79e1a08015a6
Itamar <itamar@ivc-online>
2023-04-19 14:30:14 +0300

    Make parsers look for data in correct sheet

--- quarterly_diff/parsers/altshuler_parser.py
+++ quarterly_diff/parsers/altshuler_parser.py
@@ -2,9 +2,9 @@ from .excel_parser import ExcelParser
 
 
 class Altshuler(ExcelParser):
-    COMPANIES_START_ROW = 13
+    COMPANIES_START_ROW = 12
     COMPANY_NAME_COL = 1
-    COMPANIES_ID_COL = 5
-    CURRENCY_COL = 7
-    STAKE_AT_COMPANY_COL = 8
-    STAKE_SHEET_NAME = "מניות"
+    COMPANIES_ID_COL = 4
+    CURRENCY_COL = 6
+    STAKE_AT_COMPANY_COL = 7
+    STAKE_SHEET_NAME = "לא סחיר - מניות"
